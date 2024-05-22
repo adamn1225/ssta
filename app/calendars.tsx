b53e0c6f16712c2e99753367e449e4326aef113a
Adam Noah <42944389+adamn1225@users.noreply.github.com>
2024-05-22 18:29:01 +0000

    new calendar component

--- app/calendars.tsx
+++ app/calendars.tsx
@@ -1,36 +1,41 @@
-'use client';
-import Paper from '@mui/material/Paper';
-import { ViewState } from '@devexpress/dx-react-scheduler';
+'use client'
 import {
-  Scheduler,
-  DayView,
-  Appointments,
-} from '@devexpress/dx-react-scheduler-material-ui';
+  Week, Month, Agenda, ScheduleComponent, ViewsDirective, ViewDirective, EventSettingsModel, ResourcesDirective, ResourceDirective, Inject, Resize, DragAndDrop
+} from '@syncfusion/ej2-react-schedule';
+import { timelineResourceData } from './datasource';
 
+export default function Calendar() {
+  const eventSettings: EventSettingsModel = { dataSource: timelineResourceData }
+  const group = { byGroupID: false, resources: ['Projects', 'Categories'] }
 
-
-export default async function Calendars(){
-
-  const currentDate = '2018-11-01';
-  const schedulerData = [
-    { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
-    { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
+  const projectData: Object[] = [
+    { text: 'PROJECT 1', id: 1, color: '#cb6bb2' },
+    { text: 'PROJECT 2', id: 2, color: '#56ca85' },
+    { text: 'PROJECT 3', id: 3, color: '#df5286' },
   ];
-
+  const categoryData: Object[] = [
+    { text: 'Development', id: 1, color: '#1aaa55' },
+    { text: 'Testing', id: 2, color: '#7fa900' }
+  ];
   return (
-    <Paper>
-      <Scheduler
-        data={schedulerData}
-      >
-        <ViewState
-          currentDate={currentDate}
-        />
-        <DayView
-          startDayHour={9}
-          endDayHour={14}
-        />
-        <Appointments />
-      </Scheduler>
-    </Paper>
+    <>
+      <h2>Syncfusion React Schedule Component</h2>
+      <ScheduleComponent width='100%' height='550px' currentView='Month' selectedDate={new Date(2018, 3, 4)} eventSettings={eventSettings} group={group} >
+        <ViewsDirective>
+          <ViewDirective option='Week' />
+          <ViewDirective option='Month' />
+          <ViewDirective option='Agenda' />
+        </ViewsDirective>
+        <ResourcesDirective>
+          <ResourceDirective field='ProjectId' title='Choose Project' name='Projects' allowMultiple={false}
+            dataSource={projectData} textField='text' idField='id' colorField='color'>
+          </ResourceDirective>
+          <ResourceDirective field='TaskId' title='Category' name='Categories' allowMultiple={true}
+            dataSource={categoryData} textField='text' idField='id' colorField='color'>
+          </ResourceDirective>
+        </ResourcesDirective>
+        <Inject services={[Week, Month, Agenda, Resize, DragAndDrop]} />
+      </ScheduleComponent>
+    </>
   )
-};
+}
