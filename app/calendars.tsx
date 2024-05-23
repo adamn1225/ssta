e51207e40dafb095f442932e17a88259c5f11f32
Adam Noah <42944389+adamn1225@users.noreply.github.com>
2024-05-23 19:45:12 +0000

    playing with calendar

--- app/calendars.tsx
+++ app/calendars.tsx
@@ -11,13 +11,13 @@ import {
 export default async function Calendars() {
   const currentDate = '2018-11-01';
   const schedulerData = [
-    { startDate: '2018-5-01T09:45', endDate: '2018-11-01T11:00', title: 'Guest Check-in' },
+    { startDate: '2018-11-01T09:45', endDate: '2018-11-08T11:00', title: 'Guest Check-in' },
 
 
-    { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Checkout' },
-    { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Pool Cleaing' },
-    { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Cleaning' },
-    { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Mantainance' },
+    { startDate: '2018-11-09T09:45', endDate: '2018-11-01', title: 'Checkout' },
+    { startDate: '2018-11-09T12:00', endDate: '2018-11-01', title: 'Pool Cleaing' },
+    { startDate: '2018-11-09T14:45', endDate: '2018-11-01', title: 'Cleaning' },
+    { startDate: '2018-11-09T16:00', endDate: '2018-11-01', title: 'Mantainance' },
   ];
   
   return (
