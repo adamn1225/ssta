a06d6ae67fb168aefbf41d382ca756d9e4ffc20c
Adam Noah <42944389+adamn1225@users.noreply.github.com>
2024-05-23 20:16:18 +0000

    settings page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -4,7 +4,7 @@ import TextField from '@mui/material/TextField';
 export default async function SettingsPage() {
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
-      <div className="flex items-center">
+      <div className="flex flex-col items-center">
         <h1 className="font-semibold text-lg md:text-2xl">Settings</h1>
         <p>Customize your account below.</p>
       </div>
